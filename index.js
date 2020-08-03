8fdc1995ee5af466db137fd9531618141c613ebc
siner308 <siner308@gmail.com>
2020-08-04 01:19:12 +0900

    add healthcheck

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/health', (req, res) => {
+  res.send('OK')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
