a5144b339ffceb3e9b00c0ab22ac080df420162d
siner308 <siner308@gmail.com>
2020-08-06 00:38:51 +0900

    add ip check api

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const ip = require('ip');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/ip', (req, res) => {
+  const address = ip.address();
+  res.send(address);
 })
 
 app.get('/health', (req, res) => {
